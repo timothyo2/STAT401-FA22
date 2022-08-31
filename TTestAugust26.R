227d2335c596a1138a80005deeb0a6bce3391762
timothyo2 <89275303+timothyo2@users.noreply.github.com>
2022-08-31 11:10:41 -0400

    Add files via upload

--- TTestAugust26.R
+++ TTestAugust26.R
@@ -16,3 +16,8 @@ lines(density(y), col='blue')
 #Perform T test to check if there is a significant difference between x and y variables
 ttest = t.test(x,y)
 ttest
+
+#Null hypothesis: No difference between x and y 
+#Alternate Hypothesis: significant difference between x and y (p<0.05)
+
+#Result: Accept the null hypothesis; Reject the alternate hypothesis
